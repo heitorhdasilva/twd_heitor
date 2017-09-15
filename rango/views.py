9aaf28f19f03eb195c23100a158ed4903050d543
heitor henrique <heiitorheenrique@gmail.com>
2017-09-15 15:11:55 -0300

    parte de urls feita

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    textp = """
+    <h1>Olá heitor!</h1>
+    <a href = '/rango/about'>About </a>
+    """
+    return HttpResponse(textp)
+
+def about(request):
+    texto = """
+    <h1>Sobre o Rango</h1>
+    <a href = '/rango'>Voltar</a>
+    """
+    return HttpResponse(texto)
